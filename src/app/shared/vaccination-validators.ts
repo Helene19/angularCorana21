ee8ae77e6cd08bb56aac265e8399010f7fa8ed45
Helene19 <helene.scheibmair@gmx.at>
2021-05-22 16:44:23 +0200

    delete console logs

--- src/app/shared/vaccination-validators.ts
+++ src/app/shared/vaccination-validators.ts
@@ -4,10 +4,7 @@ import { formatDate } from "@angular/common";
 export class VaccinationValidators {
 
   static dateValidator(control: FormControl): {[error: string]: any} {
-    let today = formatDate(new Date(), 'yyyy-MM-dd', 'en');
-    console.log("Look: "+control.value);
-    console.log("Yeah: "+today);
-
+    const today = formatDate(new Date(), 'yyyy-MM-dd', 'en');
     return control.value > today ? null : {dateValidator: {valid: false}};
   }
 
